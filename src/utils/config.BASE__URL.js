e0eca150bd43639430672c1dc74b21330526c40c
Jamim Mehdi <jamimmehdi@gmail.com>
2022-10-26 13:42:39 +0530

    fix: custom tab to add quotes

--- src/utils/config.BASE__URL.js
+++ src/utils/config.BASE__URL.js
@@ -1,2 +1,2 @@
-// export const BASE__URL = 'https://xerodha-backend.vercel.app'
-export const BASE__URL = 'http://localhost:4000'
+export const BASE__URL = 'https://xerodha-backend.vercel.app'
+// export const BASE__URL = 'http://localhost:4000'
